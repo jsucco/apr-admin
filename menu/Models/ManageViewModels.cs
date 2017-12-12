0374dacc8db31b3b2018c22050790345988f836e
John Joseph Succo <John.Succo@gmail.com>
2017-12-12 11:03:48 -0800

    auto set flagboard desc

--- menu/Models/ManageViewModels.cs
+++ menu/Models/ManageViewModels.cs
@@ -4,14 +4,6 @@ using Microsoft.AspNet.Identity;
 
 namespace menu.Models
 {
-    public class IndexViewModel
-    {
-        public bool HasPassword { get; set; }
-        public IList<UserLoginInfo> Logins { get; set; }
-        public string PhoneNumber { get; set; }
-        public bool TwoFactor { get; set; }
-        public bool BrowserRemembered { get; set; }
-    }
 
     public class ManageIndexViewModel
     {
